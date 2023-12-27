3b651097526dc45426aee42177494c57ab5ad3cb
Titux Metal (DEV) <github@lgdweb.fr>
2023-12-27 06:09:17 +0100

    Add some colors alias in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,21 @@
-import type { Config } from 'tailwindcss'
+import { type Config } from 'tailwindcss'
+import colors from 'tailwindcss/colors'
 
 export default {
   content: ['./app/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        background: colors.zinc['700'],
+        forground: colors.amber['100'],
+        primary: colors.blue['600'],
+        secondary: colors.amber['300'],
+        accent: colors.green['500'],
+        destructive: colors.red['400'],
+        success: colors.green['400'],
+        info: colors.sky['400']
+      }
+    }
   },
   plugins: []
 } satisfies Config
