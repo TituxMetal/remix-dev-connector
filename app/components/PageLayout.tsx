2cd7273d826debba42b265ab886426f239728633
Titux Metal (DEV) <github@lgdweb.fr>
2023-12-30 21:16:50 +0100

    Refactor PageLayout component for flexible width

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -1,17 +1,38 @@
-type PageLayoutProps = {
+import { cva, type VariantProps } from 'class-variance-authority'
+import * as React from 'react'
+
+import { cn } from '~/utils/misc'
+
+const containerVariants = cva(' flex-grow p-1', {
+  variants: {
+    fullWidth: {
+      true: 'p-0 m-auto sm:w-9/12 sm:p-2 w-full',
+      false: 'p-4 sm:p-8 md:w-3/4'
+    }
+  },
+  defaultVariants: {
+    fullWidth: false
+  }
+})
+
+interface PageLayoutProps
+  extends React.HTMLAttributes<HTMLElement>,
+    VariantProps<typeof containerVariants> {
   children: React.ReactNode
 }
 
-const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
-  return (
-    <main className='flex min-h-screen flex-col'>
-      <div className='flex flex-col items-center justify-center bg-primary/80 py-4'>Menu</div>
-      <section className='m-auto w-full flex-grow p-1 sm:w-9/12 sm:p-2'>{children}</section>
-      <div className='flex justify-center bg-primary/80 py-4'>
-        <p className='text-lg'>Created with love by TuxiMetal</p>
-      </div>
-    </main>
-  )
-}
+export const PageLayout = React.forwardRef<HTMLElement, PageLayoutProps>(
+  ({ children, className, fullWidth }, ref) => {
+    return (
+      <main ref={ref} className='flex min-h-screen flex-col'>
+        <div className='flex flex-col items-center justify-center bg-primary/80 py-4'>Menu</div>
+        <section className={cn(containerVariants({ fullWidth }), className)}>{children}</section>
+        <div className='flex justify-center bg-primary/80 py-4'>
+          <p className='text-lg'>Created with love by TuxiMetal</p>
+        </div>
+      </main>
+    )
+  }
+)
 
-export default PageLayout
+PageLayout.displayName = 'PageLayout'
